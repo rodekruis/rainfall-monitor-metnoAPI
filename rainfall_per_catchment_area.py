db883a89e9e2f9c5f434d333c53c9e86592f0d62
MishaKlein <klein.misha@gmail.com>
2022-08-31 00:43:00 +0200

    Save the TIFF file (and PNG images) after downloading rainfall. One image per timestamp
    Next task: Make heatmap of 24hr accumaltive rainfall (1day, 2day, 3day, ahead) and save as PNG

--- rainfall_per_catchment_area.py
+++ rainfall_per_catchment_area.py
@@ -9,6 +9,8 @@ Obtain a single long-format CSV-file with columns:
 -------> first version created by: Misha Klein, August 2022
 """
 from email.policy import default
+import enum
+from threading import local
 import numpy as np
 import pandas as pd
 from metno_locationforecast import Place, Forecast
@@ -24,6 +26,9 @@ import zipfile
 import yaml
 import click
 from azure.storage.blob import BlobServiceClient
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import cmocean
 
 @click.command()
 @click.option("--settings_file", type = str, required = True, default = 'settings.yml', show_default = True, help = "YAML file with global settings (input/output file names, etc.)" )
@@ -50,14 +55,20 @@ def collect_rainfall_data(settings_file, remove_temp, store_in_cloud):
     # --- unpack settings ---
     with open(settings_file,'r') as f:
         settings = yaml.safe_load(f)
-    USER_AGENT = settings['user-agent']
-    download_dir = settings['download_dir']
-    file_points_api_calls = settings['points_API_calls_file']
-    file_geotable = settings['geotable_file']
-    file_raster = settings['raster_file']
-    file_catchment_areas = settings['catchment_areas']
-    file_zonal_stats = settings['zonal_stats_catchment']
-    file_cloud = settings['cloud_file']
+    USER_AGENT = settings['METnoAPI']['user-agent']
+    download_dir = settings['METnoAPI']['download_dir']
+    file_geotable = settings['METnoAPI']['geotable_file']
+    file_points_api_calls = settings['geoCoordinates']['locations_of_interest']
+    file_catchment_areas = settings['geoCoordinates']['catchment_areas']
+    file_raster_local = settings['on_local']['tif_raw']
+    file_raster_cloud = settings['in_cloud']['tif_raw']
+    dir_png_local = settings['on_local']['png_images_dir']
+    dir_png_cloud = settings['in_cloud']['png_images_dir']
+    if not os.path.exists(dir_png_local):
+        os.mkdir(dir_png_local)
+    
+    file_zonal_stats_local = settings['on_local']['csv_zonal_stats']
+    file_zonal_stats_cloud = settings['in_cloud']['csv_zonal_stats']
 
     # -- Get predictions on grid ---
     print("weather predictions for gridpoints...")
@@ -72,8 +83,8 @@ def collect_rainfall_data(settings_file, remove_temp, store_in_cloud):
 
     # --- Save as TIF file ---
     print("save into TIF format....")
-    rainfall_array = gdf_to_rasterfile(rainfall_gdf, save_to_file=file_raster)
-    print(f"created: {file_raster}")
+    rainfall_array = gdf_to_rasterfile(rainfall_gdf, save_to_file=file_raster_local)
+    print(f"created: {file_raster_local}")
     print("--"*8 + "\n"*2)
 
     # --- perform zonal statistics ---
@@ -81,7 +92,7 @@ def collect_rainfall_data(settings_file, remove_temp, store_in_cloud):
     # get info in long-format
     rainfall_per_catchment = pd.DataFrame()
     for band_idx, timepoint in tqdm(enumerate(rainfall_array.time_of_prediction)):
-        aggregate = zonal_statistics(rasterfile=file_raster,
+        aggregate = zonal_statistics(rasterfile=file_raster_local,
                                      shapefile=file_catchment_areas,
                                      minval=0.,  # rainfall cannot be negative
                                      aggregate_by=[np.mean, np.std, np.max, np.min],
@@ -98,28 +109,38 @@ def collect_rainfall_data(settings_file, remove_temp, store_in_cloud):
         aggregate['time_of_prediction'] = pd.to_datetime(timepoint.values)
         rainfall_per_catchment = pd.concat([rainfall_per_catchment, aggregate])
     rainfall_per_catchment.reset_index(drop=True, inplace=True)
+    rainfall_per_catchment.to_csv(file_zonal_stats_local, index=False)
 
-    # save as CSV in Azure's cloud storage 
+
+
+    # ---- create image of rainfall with overlay of catchement areas --- 
+    print("creating PNG images...")
+    plot_rainfall_map(rainfall_da=rainfall_array, catchment_shapefile=file_catchment_areas, destination_fldr=dir_png_local)
+
+    # save as in Azure's cloud storage 
     if store_in_cloud:
-        write_to_azure_cloud_storage(
-            data=rainfall_per_catchment,
-            local_filename=file_zonal_stats,
-            cloud_filename=file_cloud
-        )
+        write_to_azure_cloud_storage(local_filename=file_zonal_stats_local,cloud_filename=file_zonal_stats_cloud)
+        write_to_azure_cloud_storage(local_filename=file_raster_local, cloud_filename=file_raster_cloud)
 
+        for filename in os.listdir(dir_png_local):
+            if filename.endswith('.png'):
+                write_to_azure_cloud_storage(local_filename=os.path.join(dir_png_local,filename), cloud_filename=os.path.join(dir_png_cloud, filename))
 
-        print(f"created: {file_cloud}")
+        print(f"created: {file_zonal_stats_cloud} on Azure datalake")
+        print(f"created: {file_raster_cloud} on Azure datalake")
+        print(f"wrote PNG files into: {dir_png_cloud} on Azure datalake")
         print("--"*8 + "\n"*2)
 
     # only save locally 
     else: 
-        rainfall_per_catchment.to_csv(file_zonal_stats, index=False)
-        print(f"created: {file_zonal_stats}")
+        print(f"created: {file_zonal_stats_local}")
+        print(f"created: {file_raster_local}")
+        print(f"wrote PNG files into: {dir_png_local}")
         print("--"*8 + "\n"*2)
+
     if remove_temp:
         shutil.rmtree('./temp/')
         print("removed temporary files")
-
 
     print("done")
     return
@@ -306,13 +327,50 @@ def zonal_statistics(rasterfile, shapefile,
     return zonalStats
 
 
-def write_to_azure_cloud_storage(data, local_filename, cloud_filename):
+def plot_rainfall_map(rainfall_da, catchment_shapefile, destination_fldr):
+    """
+    create colormap of rainfall in mm at one time stamp
+    will save the image into png 
+    """
+
+    # --- load in catchment area shapes --- 
+    catchmentAreas = gpd.read_file(catchment_shapefile)
+
+
+
+    rainfall_ds = rainfall_da.to_dataset('time_of_prediction')
+    rainfall_ds = rainfall_ds.rename(
+         {time:timestamp_str(time) for time in rainfall_ds}
+         )
+    for timestamp in tqdm(rainfall_da.time_of_prediction.values):
+
+        time_str = timestamp_str(timestamp)
+        plt.figure()
+        ax = plt.gca()
+        rainfall_ds[time_str].plot(ax=ax, cmap=cmocean.cm.rain, cbar_kwargs={'label':'Predicted rainfall (mm)'})
+        catchmentAreas.boundary.plot(ax = ax, color="darkgrey", linewidth = 1., linestyle='dashed')
+
+        plt.title(time_str)
+        plt.xlabel('longtitude')
+        plt.ylabel('latitude')
+        plt.xlim([34, 36])
+        sns.despine(bottom=True, left=True)
+
+        basename = timestamp_str(timestamp, fmt="%Y%m%d_%H_%M_%S")
+        filename = os.path.join(destination_fldr, f"{basename}.png")
+        plt.savefig(filename, format="png", dpi=300, bbox_inches='tight');
+ 
+
+def timestamp_str(timestamp, fmt = "%m/%d/%Y, %H:%M:%S"):
+    timestring = pd.to_datetime(timestamp)
+    return timestring.strftime(format=fmt)
+
+def write_to_azure_cloud_storage(local_filename, cloud_filename):
     """
     write resulting .csv file to cloud storrage of Azure. 
 
     data container: ibf 
     -----
-    data: Assumed to be a Pandas Dataframe that you write to CSV 
     local_filename: Path to the file on your computer (or inside Docker Container)
     cloud_filename: Path to the destination in Azure 
     """
@@ -328,7 +386,6 @@ def write_to_azure_cloud_storage(data, local_filename, cloud_filename):
     blob_client = blob_service_client.get_blob_client(container=sectrets['DataContainer'], blob=cloud_filename)
 
     # --- write data to cloud --- 
-    data.to_csv(local_filename, index=False)
     with open(local_filename, "rb") as upload_file:
         blob_client.upload_blob(upload_file, overwrite=True)
     
